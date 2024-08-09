287f17305a695fbf28dd476a868572b120829085
satendra <ersatendrasingh1325@gmail.com>
2024-08-09 07:53:13 +0530

    add: linkedlist

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -33,3 +33,191 @@ LinkedList.prototype.inserAtEnd = function (data) {
 
   last.next = newNode;
 };
+
+// insert at given node
+LinkedList.prototype.insertAfterGivenNode = function (prevNode, data) {
+  if (!prevNode) {
+    console.log(`PrevNode Cannot be null`);
+    return;
+  }
+  const newNode = new Node(data, prevNode.next);
+  prevNode.next = newNode;
+};
+
+// delete first node
+
+LinkedList.prototype.deleteFisrtNode = function () {
+  if (!this.head) {
+    console.log("Head Is Already Null");
+    return;
+  }
+
+  this.head = this.head.next;
+};
+
+// delete last node
+LinkedList.prototype.deleteLastNode = function () {
+  // first case: what if the head is null
+  if (!this.head) {
+    console.log("Head is Already Null");
+    return;
+  }
+  // second case: what if the head.next ==null
+
+  if (!this.head.next) {
+    this.head = null;
+    return;
+  }
+
+  // [6,next]->[7,next]->[8,next]->null
+
+  // let secondLast = this.head;
+  let currentLast = this.head;
+
+  while (currentLast.next.next) {
+    currentLast = currentLast.next;
+  }
+
+  currentLast.next = null;
+};
+
+// delete node at given point
+
+LinkedList.prototype.deleteNodeAtGivenKey = function (key) {
+  if (!this.head) {
+    console.log("LinkedList IS Already Empty");
+  }
+
+  // data found at head
+  if (this.head.data === key) {
+    this.head = this.head.next;
+    return;
+  }
+
+  let currentNode = this.head;
+
+  while (currentNode.next !== null) {
+    if (currentNode.next.data === key) {
+      currentNode.next = currentNode.next.next;
+      return;
+    }
+    currentNode = currentNode.next;
+  }
+
+  console.log(`Key Not Found in linkdlist:${key}`);
+};
+
+// Search return (true false)
+
+LinkedList.prototype.search = function (key) {
+  if (!this.head) {
+    console.log("LinkedList Is Empty");
+    return;
+  }
+
+  if (this.head.data === key) {
+    console.log("Data found at the head node ");
+    return;
+  }
+
+  let currentNode = this.head;
+  let count = 0;
+
+  while (currentNode) {
+    if (currentNode.data === key) {
+      console.log(`Data found at the ${count} node`);
+      return true;
+    }
+    currentNode = currentNode.next;
+    count++;
+  }
+
+  console.log(`Give Data not found in the linkedlist`);
+  return false;
+};
+
+// search return node
+
+LinkedList.prototype.searchNode = function (key) {
+  let currentNode = this.head;
+
+  while (currentNode) {
+    if (currentNode.data === key) {
+      return currentNode;
+    }
+    currentNode = currentNode.next;
+  }
+
+  console.log(`Data not found in the linked list: ${key}`);
+  return null;
+};
+
+// Travesel
+
+LinkedList.prototype.printList = function () {
+  let listValue = [];
+  let current = this.head;
+
+  while (current) {
+    listValue.push(current.data);
+    current = current.next;
+  }
+
+  if (listValue.length === 0) {
+    console.log("LinkedList is Empty");
+    return;
+  }
+
+  console.log(listValue.join(" -> "));
+};
+
+// Reverse A linkedList
+
+LinkedList.prototype.reverse = function () {
+  let prev = null;
+  let curr = this.head;
+  let next = null;
+
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  this.head = prev;
+};
+
+const linkedList = new LinkedList();
+
+linkedList.inserAtEnd(1);
+linkedList.inserAtEnd(2);
+linkedList.inserAtEnd(3);
+linkedList.inserAtEnd(4);
+linkedList.inserAtEnd(5);
+
+console.log("Before Delete");
+linkedList.printList();
+
+linkedList.deleteFisrtNode();
+linkedList.deleteLastNode();
+linkedList.deleteNodeAtGivenKey(3);
+
+// linkdlist.search(55);
+
+console.log("After Delete");
+
+linkedList.printList();
+
+console.log("Insert");
+
+const node = linkedList.searchNode(4);
+if (node) {
+  linkedList.insertAfterGivenNode(node, 20);
+}
+linkedList.printList();
+
+console.log("Reverse");
+
+linkedList.reverse();
+linkedList.printList();
